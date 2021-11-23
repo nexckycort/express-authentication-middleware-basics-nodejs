bd1ebed5a2881c1ff821a1c1f0290f1cada640e7
Nestor Cortina <junior110120@gmail.com>
2021-11-23 04:05:32 -0500

    feat: implementacion del middleware auth

--- middleware.js
+++ middleware.js
@@ -1,5 +1,36 @@
-const VALID_KEYS_PATH = __dirname + '/valid-keys.txt';
+const fs = require("fs");
+const os = require("os");
+const { compact } = require("lodash");
 
-module.exports = function (req, res, next) {
+const VALID_KEYS_PATH = __dirname + "/valid-keys.txt";
 
+const readFile = (path) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, { encoding: "utf-8" }, (err, data) => {
+      if (err) reject(err);
+      else resolve(data);
+    });
+  });
+};
+
+const getKeysFromFile = () => {
+  return readFile(VALID_KEYS_PATH).then((data) => {
+    return compact(data.split(os.EOL));
+  });
+};
+
+const unauthorized = (res) => res.status(401);
+
+module.exports = async function auth(req, res, next) {
+  try {
+    const apiKey = req.get("x-api-key");
+    if (apiKey === undefined) return unauthorized(res);
+
+    const apiKeys = await getKeysFromFile();
+    if (!apiKeys.includes(apiKey)) return unauthorized(res);
+    next();
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "internal error" });
+  }
 };
